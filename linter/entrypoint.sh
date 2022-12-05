e65c8dc12cdfde34c5b9314755a6edeaa128795f
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-12-05 22:59:23 +0100

    fix(CI): lint new fsproj

--- linter/entrypoint.sh
+++ linter/entrypoint.sh
@@ -2,6 +2,6 @@ dotnet tool install -g dotnet-fsharplint
 # I'm not 100% sure what the canonical approach is when it comes
 # to linting dotnet projects, but if we restore it's possible to pass
 # in a project file to the linter.
-dotnet restore
+dotnet restore $@
 
 dotnet-fsharplint lint $@
